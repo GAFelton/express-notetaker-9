c1043e8ffd60951b06ba0a1f905e44f488a5458b
George F <64555318+GAFelton@users.noreply.github.com>
2020-08-27 12:57:15 -0700

    Added readFile route.

---
+++ routes/apiRoutes.js
@@ -0,0 +1,18 @@
+
+var db = require("../Develop/db/db.json");
+
+const apiRoutes = (app, fs) => {
+
+    app.get(db, (req, res) => {
+        fs.readFile(db, "utf8", (err, data) => {
+            if (err) {
+                throw err;
+            }
+
+            res.send(JSON.parse(data));
+        })
+    })
+
+};
+
+module.exports = apiRoutes;
