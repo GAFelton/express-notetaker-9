f46750054ce70352ce5d751390f08591a088963b
George F <64555318+GAFelton@users.noreply.github.com>
2020-08-27 12:49:02 -0700

    Basic server set up.

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./routes/apiRoutes")(app, fs);
+require("./routes/htmlRoutes")(app, fs);
+
+app.listen(PORT, () => {
+  console.log("App listening on port:" + PORT);
+});
