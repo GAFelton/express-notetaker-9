c95a81826f732474e2773fb8588436ebf027d422
George F <64555318+GAFelton@users.noreply.github.com>
2020-08-30 12:02:52 -0700

    Commented out static declaration.

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// app.use(express.static('assets'));
+
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
